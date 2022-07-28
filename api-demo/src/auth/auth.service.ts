f7373c369533da47a69bed2a5994ee4a2e89962d
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-07-27 21:41:38 -0300

    fix: Global config JWT strategy

--- api-demo/src/auth/auth.service.ts
+++ api-demo/src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string): Promise<User | null> {
     const userFind = await this.usersService.findOne(email);
@@ -12,5 +16,16 @@ export class AuthService {
       return userFind;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      role: user.role,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
---
+++ api-demo/src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'algo muy dificil de leer',
+};
